1b1a24f9d934e4261bf590e01f9263a49f15b9b9
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-07-28 03:34:38 +0530

    save

--- web/src/app/myAccount/trainers/profile/profile.component.ts
+++ web/src/app/myAccount/trainers/profile/profile.component.ts
@@ -6,39 +6,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   activeProfileTab: number = 0;
-  expandedTab:number = -1;
+  expandedTab: number = -1;
   scrollTop;
   tabArray = []
   constructor() { }
 
   ngOnInit() {
     this.tabArray = [
-      { 'name': 'Personal' },
-      { 'name': 'Credentials' },
-      { 'name': 'Keywords' },
-      { 'name': 'About' },
-      { 'name': 'Certifications Earned' },
-      { 'name': 'Social Media' }
+      { 'name': 'Personal', 'label': 'Personal', 'displayOrder': '0' },
+      { 'name': 'Credentials', 'label': 'Password', 'displayOrder': '1' },
+      { 'name': 'Keywords', 'label': 'Keywords', 'displayOrder': '2' },
+      { 'name': 'About', 'label': 'About', 'displayOrder': '3' },
+      { 'name': 'Certifications Earned', 'label': 'Certifications Earned', 'displayOrder': '4' },
+      { 'name': 'Social Media', 'label': 'Social Media', 'displayOrder': '5' }
     ]
   }
   activeTab = (number, className: string): void => {
-    this.activeProfileTab= number;
+    this.activeProfileTab = number;
     const elementList = document.querySelectorAll('.' + className);
-      let element;
-      if (number == -1) {
-        element = elementList[0] as HTMLElement;
-      } else {
-        element = elementList[number] as HTMLElement;
-      }
-      setTimeout(() => {
-        const yCoordinate = element.getBoundingClientRect().top + window.pageYOffset;
+    let element;
+    if (number == -1) {
+      element = elementList[0] as HTMLElement;
+    } else {
+      element = elementList[number] as HTMLElement;
+    }
+    setTimeout(() => {
+      const yCoordinate = element.getBoundingClientRect().top + window.pageYOffset;
       const yOffset = -50;
 
       window.scrollTo({
         top: yCoordinate + yOffset,
         behavior: 'smooth'
       });
-      }, 300);
+    }, 300);
 
   }
 }
