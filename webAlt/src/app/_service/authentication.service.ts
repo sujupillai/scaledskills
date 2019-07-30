3d656f51bcd6ebd04d4561e4b0aa21977f995bff
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-07-30 21:05:23 +0530

    add

--- webAlt/src/app/_service/authentication.service.ts
+++ webAlt/src/app/_service/authentication.service.ts
@@ -37,5 +37,5 @@ export class AuthenticationService {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
         this.currentUserSubject.next(null);
-    }
+    }   
 }
