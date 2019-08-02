5499df2ad778c1772520cae8cac2f8f9c31e5a19
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-03 01:00:00 +0530

    password form

--- web/src/app/myAccount/trainers/profile/credentials/credentials.component.ts
+++ web/src/app/myAccount/trainers/profile/credentials/credentials.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-credentials',
   templateUrl: './credentials.component.html'
 })
 export class CredentialsComponent implements OnInit {
-
-  constructor() { }
+  credentialForm: FormGroup;
+  constructor(private _FormBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm(() => {
+
+    })
+  }
+  createForm = (callback: any): void => {
+    this.credentialForm = this._FormBuilder.group({
+      'currentPassword': ['', Validators.required],
+      'newPassword': ['', Validators.required],
+      'confirmPassword': ['', Validators.required],
+    })
+    if (callback) {
+      callback()
+    }
+  }
+  handleSubmit = () => {
+    let postObj = {
+      ...this.credentialForm.value
+    }
+    prompt('postObj', JSON.stringify(postObj))
   }
 
 }
