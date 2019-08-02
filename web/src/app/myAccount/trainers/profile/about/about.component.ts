6d8ba3f0c0a6afd2dc0800f10419f158dc6e3ad5
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-03 02:14:06 +0530

    about form

--- web/src/app/myAccount/trainers/profile/about/about.component.ts
+++ web/src/app/myAccount/trainers/profile/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _FormBuilder: FormBuilder) { }
+  aboutForm: FormGroup
   ngOnInit() {
+    this.createForm(() => { })
+  }
+  createForm = (callback) => {
+    this.aboutForm = this._FormBuilder.group(
+      {
+        'profileURL': ['', Validators.required],
+        'bio': ['', Validators.required],
+        'language': ['', Validators.required],
+
+      }
+    )
+    if (callback) {
+      callback()
+    }
+  }
+  handleSubmit = (): void => {
+    let postData = {
+      ...this.aboutForm.value
+    }
+    prompt('postData', JSON.stringify(postData))
   }
 
 }
