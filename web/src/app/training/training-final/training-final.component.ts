05378331a3667ffcfe8bf59af5d494d13e06c4de
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-03 23:11:48 +0530

    trainig final component

--- web/src/app/training/training-final/training-final.component.ts
+++ web/src/app/training/training-final/training-final.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-training-final',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training-final.component.scss']
 })
 export class TrainingFinalComponent implements OnInit {
-
-  constructor() { }
+  trainingAgreementForm: FormGroup
+  constructor(private _FormBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm(() => { })
+  }
+  createForm = (callback) => {
+    this.trainingAgreementForm = this._FormBuilder.group({
+      isAgree: false
+    })
+    if (callback) {
+      callback()
+    }
   }
 
 }
