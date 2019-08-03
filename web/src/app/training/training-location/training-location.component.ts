2f6c1aa67e6a0abbd83e15100b5103303c59af68
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-03 23:00:37 +0530

    traning page module

--- web/src/app/training/training-location/training-location.component.ts
+++ web/src/app/training/training-location/training-location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-training-location',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training-location.component.scss']
 })
 export class TrainingLocationComponent implements OnInit {
-
-  constructor() { }
+  trainingLocationForm: FormGroup;
+  constructor(private _FormBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm(() => { })
+  }
+  createForm = (callback) => {
+    this.trainingLocationForm = this._FormBuilder.group({
+      location: ['', Validators.required]
+    })
+    if (callback) {
+      callback()
+    }
+  }
+  get formControl(){return this.trainingLocationForm.controls};
+  handleSubmit = () => {
+    let postData = {
+      ...this.trainingLocationForm.value
+    }
+    prompt('postData', JSON.stringify(postData))
   }
 
 }
