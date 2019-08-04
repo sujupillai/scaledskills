c8d92dfe68b2eaab3c9b1ab7071882e47400cb95
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-04 20:46:24 +0530

    review

--- web/src/app/training/training-review/training-review.component.ts
+++ web/src/app/training/training-review/training-review.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-training-review',
   templateUrl: './training-review.component.html',
   styleUrls: ['./training-review.component.scss']
 })
 export class TrainingReviewComponent implements OnInit {
-
-  constructor() { }
-
+  traineeReviewForm: FormGroup
+  constructor(private _FormBuilder: FormBuilder) { }
+  @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
   ngOnInit() {
+    this.createForm(() => { })
+  }
+  createForm = (callback) => {
+    this.traineeReviewForm = this._FormBuilder.group({
+
+    })
   }
 
 }
