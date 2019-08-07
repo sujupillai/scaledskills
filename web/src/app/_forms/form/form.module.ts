9a52f2b4ac1ae1cc2c8b5fc8e146f96a2f72cb0f
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-07 22:36:23 +0530

    form module

--- web/src/app/_forms/form/form.module.ts
+++ web/src/app/_forms/form/form.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MaterialModule } from '../../_material/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatNativeDateModule , MaterialModule, ReactiveFormsModule
+    CommonModule, MatNativeDateModule, MaterialModule, ReactiveFormsModule, FormsModule
   ],
-  exports:[
-    CommonModule, MatNativeDateModule , MaterialModule, ReactiveFormsModule
+  exports: [
+    CommonModule, MatNativeDateModule, MaterialModule, ReactiveFormsModule, FormsModule
   ]
 })
 export class FormModule { }
--- web/src/app/myAccount/trainers/trainers.module.ts
+++ web/src/app/myAccount/trainers/trainers.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TrainersRoutingModule } from './trainers-routing.module';
 import { ProfileComponent } from './profile/profile.component';
-import { MaterialModule } from '../../_material/material.module';
 import { PersonalComponent } from './profile/personal/personal.component';
 import { CredentialsComponent } from './profile/credentials/credentials.component';
 import { KeywordsComponent } from './profile/keywords/keywords.component';
 import { AboutComponent } from './profile/about/about.component';
 import { CertificationsEarnedComponent } from './profile/certifications-earned/certifications-earned.component';
 import { SocialMediaComponent } from './profile/social-media/social-media.component';
+import {FormModule} from '../../_forms/form/form.module'
 
 @NgModule({
   declarations: [ProfileComponent, PersonalComponent, CredentialsComponent, KeywordsComponent, AboutComponent, CertificationsEarnedComponent, SocialMediaComponent],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     TrainersRoutingModule,
-    MaterialModule
+    FormModule
   ]
 })
 export class TrainersModule { }
