6e912607e3790d5c7aedd482dcbca3ebeb14ae32
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-12 23:43:29 +0530

    code cleanup

--- web/src/app/_shared/header/header.component.ts
+++ web/src/app/_shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../_service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  userInfo: any = {};
+  isLoggedIn: boolean = false;
+  constructor(private _AuthenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.gerUserInfo();
+    this._AuthenticationService.getAuthEmitter().subscribe((customObject) => {
+      this.gerUserInfo();
+    });
   }
+  handleLogOut = () => {
+    this._AuthenticationService.logout()
+    this.gerUserInfo();
+  }
+  gerUserInfo = () => {
+    this.userInfo = this._AuthenticationService.currentUserValue
+    this.isLoggedIn = this.userInfo ? true : false;
+  }
+
 
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,21 +10,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { FooterComponent } from './_shared/footer/footer.component';
-import { HeaderTopComponent } from './_shared/header/header-top/header-top.component';
-import { HeaderMiddleComponent } from './_shared/header/header-middle/header-middle.component';
-import { HeaderFullComponent } from './_shared/header/header-full/header-full.component';
+
 import { ErrorComponent } from './_shared/error/error.component';
-import { HeaderSearchComponent } from './_shared/header/header-search/header-search.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HeaderTopComponent,
-    HeaderFullComponent,
-    HeaderMiddleComponent,
-    ErrorComponent,
-    HeaderSearchComponent
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
