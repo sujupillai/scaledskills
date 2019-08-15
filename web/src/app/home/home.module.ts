1f8969cd168762ba0e055c569946581c0db8e496
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-15 12:58:51 +0530

    save

--- web/src/app/home/home.module.ts
+++ web/src/app/home/home.module.ts
@@ -5,10 +5,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NguCarouselModule } from '@ngu/carousel';
 import { AboutComponent } from './about/about.component';
 import { CategoryComponent } from './home/category/category.component';
-import { HomeProgressComponent } from './home/home-progress/home-progress.component';
-import { HomeWorkshopComponent } from './home/home-workshop/home-workshop.component';
 import { HomeSearchComponent } from './home/home-search/home-search.component';
-import {FormModule} from '../_forms/form/form.module';
+import { FormModule } from '../_forms/form/form.module';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { TermsUseComponent } from './terms-use/terms-use.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
@@ -17,7 +15,7 @@ import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { TestimonialComponent } from './testimonial/testimonial.component'
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, CategoryComponent, HomeProgressComponent, HomeWorkshopComponent, HomeSearchComponent, ContactUsComponent, TermsUseComponent, PrivacyPolicyComponent, HomeAddComponent, HomeBannerComponent, TestimonialComponent],
+  declarations: [HomeComponent, AboutComponent, CategoryComponent, HomeSearchComponent, ContactUsComponent, TermsUseComponent, PrivacyPolicyComponent, HomeAddComponent, HomeBannerComponent, TestimonialComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
