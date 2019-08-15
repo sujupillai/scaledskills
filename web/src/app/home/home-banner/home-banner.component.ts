e3c32e07183505b8d01e6620ab1805ab3e9524fd
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-15 13:43:40 +0530

    save

--- web/src/app/home/home-banner/home-banner.component.ts
+++ web/src/app/home/home-banner/home-banner.component.ts
@@ -13,7 +13,10 @@ export class HomeBannerComponent implements OnInit, AfterViewInit {
     interval: { timing: 4000, initialDelay: 1000 },
     loop: true,
     touch: true,
-    velocity: 0.2
+    velocity: 0.2,
+    point: {
+      visible: true
+    },
   }
   carouselItems = [
     {
@@ -31,3 +34,39 @@ export class HomeBannerComponent implements OnInit, AfterViewInit {
     this.cdr.detectChanges();
   }
 }
+
+
+
+// import { Component,  ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit } from '@angular/core';
+// import { NguCarouselConfig } from '@ngu/carousel';
+// @Component({
+//   selector: 'app-home-banner',
+//   templateUrl: './home-banner.component.html',
+//   styleUrls: ['./home-banner.component.scss'],
+//   changeDetection: ChangeDetectionStrategy.OnPush
+// })
+
+// export class HomeBannerComponent implements AfterViewInit {
+//   name = 'Angular';
+//   slideNo = 0;
+//   withAnim = true;
+//   resetAnim = true;
+
+//   carouselConfig: NguCarouselConfig = {
+//     grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+//     load: 3,
+//     interval: {timing: 4000, initialDelay: 1000},
+//     loop: true,
+//     touch: true,
+//     velocity: 0.2
+//   }
+//   carouselItems = [1, 2, 3];
+
+//   constructor(private cdr: ChangeDetectorRef) {}
+
+//   ngAfterViewInit() {
+//     this.cdr.detectChanges();
+//   }
+
+
+// }
