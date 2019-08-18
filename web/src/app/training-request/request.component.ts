941d11785dd098abc2c7c5b970a609e45b810a3e
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-18 19:45:37 +0530

    save

--- web/src/app/training-request/request.component.ts
+++ web/src/app/training-request/request.component.ts
@@ -1,4 +1,5 @@
-
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
@@ -9,6 +10,12 @@ import { AngularEditorConfig } from '@kolkov/angular-editor';
 })
 export class RequestComponent implements OnInit {
   requestForm: FormGroup;
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  keywordList = [];
   descriptionConfig: AngularEditorConfig = {
     editable: true,
     spellcheck: true,
@@ -31,13 +38,39 @@ export class RequestComponent implements OnInit {
       trainingType: ['', Validators.required],
       companyName: ['', Validators.required],
       trainingSummary: ['', Validators.required],
+      participant: [''],
       description: ['', Validators.required],
+      address: this._FormBuilder.group({
+        city: [''],
+        zipCode: [''],
+        street: [''],
+        countryId: [''],
+        stateId: [''],
+      }),
+      budget: [''],
+      keywords: [''],
     })
     if (callback) {
       callback()
     }
   }
   get formControl() { return this.requestForm.controls }
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+    if ((value || '').trim()) {
+      this.keywordList.push({ name: value.trim() });
+    }
+    if (input) {
+      input.value = '';
+    }
+  }
+  remove(item): void {
+    const index = this.keywordList.indexOf(item);
+    if (index >= 0) {
+      this.keywordList.splice(index, 1);
+    }
+  }
   handleSubmit = () => {
     let postObj = {
       ...this.requestForm.value
