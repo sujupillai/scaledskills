ef80b1b1f630fc35e1c7c080bbccdd84ffa85833
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-18 11:32:54 +0530

    save

--- web/src/app/auth/register/register.component.ts
+++ web/src/app/auth/register/register.component.ts
@@ -7,8 +7,7 @@ import { ConfirmationDialogComponent } from '../../_shared/confirmation-dialog/c
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup
