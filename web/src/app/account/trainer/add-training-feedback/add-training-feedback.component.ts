340411f4ae4b8e4c1548089d16466a9e1ff49d5b
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-08-23 16:35:28 +0530

    save

--- web/src/app/account/trainer/add-training-feedback/add-training-feedback.component.ts
+++ web/src/app/account/trainer/add-training-feedback/add-training-feedback.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-add-training-feedback',
   templateUrl: './add-training-feedback.component.html'
 })
 export class AddTrainingFeedbackComponent implements OnInit {
-
-  constructor() { }
-
+  traineeReviewForm: FormGroup
+  constructor(private _FormBuilder: FormBuilder) { }
+  @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
   ngOnInit() {
+    this.createForm(() => { })
   }
-
+  createForm = (callback) => {
+    this.traineeReviewForm = this._FormBuilder.group({
+    })
+  }
 }
--- web/src/app/account/trainer/add-training-settings/add-training-settings.component.ts
+++ web/src/app/account/trainer/add-training-settings/add-training-settings.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-add-training-settings',
   templateUrl: './add-training-settings.component.html'
 })
 export class AddTrainingSettingsComponent implements OnInit {
-
-  constructor() { }
-
+  trainingSettingForm: FormGroup;
+  constructor(private _FormBuilder: FormBuilder) { }
+  optionArray = [];
   ngOnInit() {
+    this.createForm(() => { })
   }
-
+  createForm = (callback) => {
+    this.trainingSettingForm = this._FormBuilder.group({
+      customQuestions: false,
+      remainingTickets: false,
+      setReminder: false,
+      affiliatePromoterLink: false,
+      trainingId: false,
+      pageViews: false,
+      interestedUsers: false,
+      registeredUsers: false,
+      addToCalendar: false,
+      xSpotsLeft: false,
+      comments: false,
+      feedback: false,
+      aboutOrganizer: false,
+      moreEventsFromSameOrganizer: false,
+    })
+    if (callback) {
+      callback()
+    }
+  }
 }
