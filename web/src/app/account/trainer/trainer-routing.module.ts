46ba81141a724587696f315624db51dd6f0adaf6
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-09-08 22:01:02 +0530

    save

--- web/src/app/account/trainer/trainer-routing.module.ts
+++ web/src/app/account/trainer/trainer-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TrainerComponent } from './trainer.component'
 import { TrainerProfileComponent } from './trainer-profile/trainer-profile.component';
-import { OrgBasicComponent } from '../../_shared/profile/org-basic/org-basic.component';
+import { BasicComponent } from '../../_shared/profile/basic/basic.component';
 import { OrgAboutComponent } from '../../_shared/profile/org-about/org-about.component';
 import { SocialComponent } from '../../_shared/profile/social/social.component';
 import { KeywordsComponent } from '../../_shared/profile/keywords/keywords.component';
@@ -27,8 +27,7 @@ const routes = [
         path: 'profile', component: TrainerProfileComponent,
         children: [
           { path: '', redirectTo: 'basic', pathMatch: 'full' },
-          { path: 'basic', component: OrgBasicComponent },
-          { path: 'keyword', component: KeywordsComponent },
+          { path: 'basic', component: BasicComponent },
           { path: 'about', component: OrgAboutComponent },
           { path: 'certificate', component: CertificationsComponent },
           { path: 'social', component: SocialComponent },
