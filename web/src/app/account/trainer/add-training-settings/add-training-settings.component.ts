6f03376d674e61272468b5c167061f6d17c0163f
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-09-29 08:29:37 +0530

    setting

--- web/src/app/account/trainer/add-training-settings/add-training-settings.component.ts
+++ web/src/app/account/trainer/add-training-settings/add-training-settings.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 import { ApiPath } from '../../../_helpers/_constants/api';
 import { HttpService, SharedService } from '../../../_service'
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-add-training-settings',
   templateUrl: './add-training-settings.component.html'
 })
 export class AddTrainingSettingsComponent implements OnInit {
-  trainingSettingForm: FormGroup;
-  constructor(private _FormBuilder: FormBuilder, private _SharedService: SharedService, private _HttpService: HttpService) { }
+  form: FormGroup;
+  trainingId: any = 0;
+  prevState
+  constructor(private _FormBuilder: FormBuilder, private _SharedService: SharedService, private _HttpService: HttpService, private _ActivatedRoute: ActivatedRoute, private _Router: Router) { }
   optionArray = [];
   ngOnInit() {
+    this._ActivatedRoute.parent.params.subscribe((param: any) => {
+      this.trainingId = param.id;
+      if (this.trainingId == 0) {
+        let msgArray = [
+          { mgs: 'Sorry! You have to create a training first', class: 'confirmMsg' },
+        ]
+        this._SharedService.dialogConfig(msgArray, true, true, false, 'OKAY', 'CANCEL', false, 'Alert').subscribe(res => {
+          if (res == 1) {
+            this._Router.navigate(['account/trainer/training/0/basic']);
+          }
+        })
+        return
+      } else {
+        this.getData()
+      }
+    });
     this.createForm(() => { })
   }
   createForm = (callback) => {
-    this.trainingSettingForm = this._FormBuilder.group({
-      trainingId: 0,
+    this.form = this._FormBuilder.group({
+      trainingId: this.trainingId,
       id: 0,
       isPublicRepeat: false,
       isPublicCustomQuestion: false,
@@ -37,11 +56,15 @@ export class AddTrainingSettingsComponent implements OnInit {
       callback()
     }
   }
-  get formControl() { return this.trainingSettingForm.controls };
+  get formControl() { return this.form.controls };
   getData = () => {
-    const url = ApiPath.trainingSettings;
+    let url = ApiPath.trainingSettings;
+    url = url.replace('{TrainingId}', this.trainingId.toString())
     this._HttpService.httpCall(url, 'GET', null, null).subscribe(res => {
-      let resObj = res;
+      this.prevState = {
+        ...res.result
+      };
+      let resObj = res.result;
       Object.keys(resObj).forEach(name => {
         if (this.formControl[name]) {
           this.formControl[name].setValue(resObj[name]);
@@ -50,9 +73,10 @@ export class AddTrainingSettingsComponent implements OnInit {
     })
   }
   handleSubmit = () => {
-    const url = ApiPath.trainingSettings;
+    let url = ApiPath.trainingSettings;
+    url = url.replace('{TrainingId}', this.trainingId.toString())
     let postObj = {
-      ...this.trainingSettingForm.value
+      ...this.form.value
     }
     this._HttpService.httpCall(url, 'POST', postObj, null).subscribe(res => {
       if (res.responseCode == 200) {
@@ -81,5 +105,26 @@ export class AddTrainingSettingsComponent implements OnInit {
         // dialogConfig(mesage, isAction, isYes, isNo, yesText, noText, autoClose, header)
         this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Error')
       });
+  }
+  resetForm(formGroup: FormGroup) {
+    formGroup.reset();
+    formGroup.markAsUntouched();
+    Object.keys(this.prevState).forEach(name => {
+      if (this.formControl[name]) {
+        this.formControl[name].setValue(this.prevState[name]);
+      }
+    });
+  }
+  handleCancel = () => {
+    let msgArray = [
+      { mgs: 'Are you sure, you want to cancel ?', class: 'confirmMsg' },
+      { mgs: 'Unsaved changes will not be saved.', class: 'subMsg' },
+    ]
+    // dialogConfig(mesage, isAction, isYes, isNo, yesText, noText, autoClose, header)
+    this._SharedService.dialogConfig(msgArray, true, true, true, 'YES', 'CANCEL', false, 'Sucess').subscribe(res => {
+      if (res == 1) {
+        this.resetForm(this.form)
+      }
+    })
   }
 }
