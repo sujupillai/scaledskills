e0438297ed2242f6b480b1fb187b17b450a642cc
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-10-13 03:22:36 +0530

    save

--- web/src/app/_shared/profile/org-basic/org-basic.component.ts
+++ web/src/app/_shared/profile/org-basic/org-basic.component.ts
@@ -14,18 +14,21 @@ export class OrgBasicComponent implements OnInit {
   selectedCountry = [];
   selectedState = [];
   settings = {};
-  defaultList = [];
+  defaultList = [{
+    "text": "Select",
+    "value": "0",
+    "isSelect": false
+  }];
+  countryList = [];
+  stateList = [];
   isCopied: boolean = false;
   constructor(private _FormBuilder: FormBuilder, private _HttpService: HttpService, private _SharedService: SharedService) { }
   ngOnInit() {
     this.createForm(() => {
       this.getCountryList();
-      this.defaultList = [{
-        "text": "Select",
-        "value": "0",
-        "isSelect": false
-      }];
-      this.settings = { singleSelection: true, text: "Select", labelKey: "text", primaryKey: "value", noDataLabel: 'No items' };
+      this.settings = {
+        singleSelection: true, text: "Select", labelKey: "text", primaryKey: "value", classes: "myclass custom-class", enableSearchFilter: true, searchBy: ['text'], searchPlaceholderText: 'Search by name'
+      };
       this.fetchData()
     })
   }
@@ -39,7 +42,7 @@ export class OrgBasicComponent implements OnInit {
       phoneNumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       gst: ['', [Validators.minLength(15), Validators.maxLength(15)]],
       panNumber: ['', [Validators.minLength(10), Validators.maxLength(10)]],
-      idProof: ['', [Validators.required]],
+      idProof: [''],
       address: this._FormBuilder.group({
         address1: [''],
         address2: [''],
@@ -58,6 +61,24 @@ export class OrgBasicComponent implements OnInit {
     }
   }
   get formControl() { return this.formElement.controls };
+  setAddress = (dataObj) => {
+    this.formElement.get(['address', 'address1']).setValue(dataObj.address && dataObj.address.address1 ? dataObj.address.address1 : 'NA');
+    this.formElement.get(['address', 'address2']).setValue(dataObj.address && dataObj.address.address2 ? dataObj.address.address2 : 'NA');
+    this.formElement.get(['address', 'address3']).setValue(dataObj.address && dataObj.address.address3 ? dataObj.address.address3 : 'NA');
+    this.formElement.get(['address', 'city']).setValue(dataObj.address && dataObj.address.city ? dataObj.address.city : 'NA');
+    this.formElement.get(['address', 'zipCode']).setValue(dataObj.address && dataObj.address.zipCode ? dataObj.address.zipCode : 'NA');
+    this.formElement.get(['address', 'street']).setValue(dataObj.address && dataObj.address.street ? dataObj.address.street : 'NA');
+    this.formElement.get(['address', 'countryId']).setValue(dataObj.address && dataObj.address.countryId ? dataObj.address.countryId : 0);
+    this.formElement.get(['address', 'stateId']).setValue(dataObj.address && dataObj.address.stateId ? dataObj.address.stateId : 0);
+    this.formElement.get(['address', 'countryObj']).setValue(dataObj.address && dataObj.address.countryObj ? dataObj.address.countryObj : 'NA');
+    this.formElement.get(['address', 'stateObj']).setValue(dataObj.address && dataObj.address.stateObj ? dataObj.address.stateObj : 'NA');
+    this.selectedCountry = dataObj.address && dataObj.address.countryObj ? [dataObj.address.countryObj] : this.defaultList;
+    this.selectedState = dataObj.address && dataObj.address.stateObj ? [dataObj.address.stateObj] : this.defaultList;
+    if (this.formElement.get(['address', 'countryId']).value > 0) {
+      let id = this.formElement.get(['address', 'countryId']).value
+      this.getStateList(id)
+    }
+  }
   fetchData = () => {
     let url = ApiPath.Organization;
     this._HttpService.httpCall(url, 'GET', null, null).pipe(first()).subscribe(res => {
@@ -72,22 +93,7 @@ export class OrgBasicComponent implements OnInit {
         });
         let urlStr = (this.formControl['name'].value).split(' ').join('_')
         this.formControl['profileUrl'].setValue(urlStr)
-        this.formElement.get(['address', 'address1']).setValue(dataObj.address && dataObj.address.address1 ? dataObj.address.address1 : 'NA');
-        this.formElement.get(['address', 'address2']).setValue(dataObj.address && dataObj.address.address2 ? dataObj.address.address2 : 'NA');
-        this.formElement.get(['address', 'address3']).setValue(dataObj.address && dataObj.address.address3 ? dataObj.address.address3 : 'NA');
-        this.formElement.get(['address', 'city']).setValue(dataObj.address && dataObj.address.city ? dataObj.address.city : 'NA');
-        this.formElement.get(['address', 'zipCode']).setValue(dataObj.address && dataObj.address.zipCode ? dataObj.address.zipCode : 'NA');
-        this.formElement.get(['address', 'street']).setValue(dataObj.address && dataObj.address.street ? dataObj.address.street : 'NA');
-        this.formElement.get(['address', 'countryId']).setValue(dataObj.address && dataObj.address.countryId ? dataObj.address.countryId : 'NA');
-        this.formElement.get(['address', 'stateId']).setValue(dataObj.address && dataObj.address.stateId ? dataObj.address.stateId : 'NA');
-        this.formElement.get(['address', 'countryObj']).setValue(dataObj.address && dataObj.address.countryObj ? dataObj.address.countryObj : 'NA');
-        this.formElement.get(['address', 'stateObj']).setValue(dataObj.address && dataObj.address.stateObj ? dataObj.address.stateObj : 'NA');
-        this.selectedCountry = [dataObj.address && dataObj.address.countryObj ? dataObj.address.countryObj : this.defaultList];
-        this.selectedState = [dataObj.address && dataObj.address.stateObj ? dataObj.address.stateObj : this.defaultList];
-        if (dataObj.address && dataObj.address.countryId > 0) {
-          let id = this.formElement.get(['address', 'countryId']).value
-          this.getStateList(id)
-        }
+        this.setAddress(dataObj)
       }
     })
   }
@@ -105,6 +111,31 @@ export class OrgBasicComponent implements OnInit {
   getStateList = (id) => {
     const url = ApiPath.globalState + '/' + id;
     this.getMaster(url, 'stateList')
+  }
+  OnCountrySelect(event) {
+    let id = event.value
+    this.formElement.get(['address', 'countryId']).setValue(event.value)
+    this.getStateList(id)
+  }
+  OnCountryDeSelect(event) {
+    this.formElement.get(['address', 'countryId']).setValue('')
+    this.formElement.get(['address', 'stateId']).setValue('')
+    this.formElement.get(['address', 'stateObj']).setValue('')
+    this.formElement.get(['address', 'city']).setValue('');
+    this.formElement.get(['address', 'zipCode']).setValue('');
+    this.selectedState = this.defaultList;
+    this.selectedState = this.defaultList;
+    this.stateList = [];
+  }
+  onChangeState(event) {
+    this.formElement.get(['address', 'stateId']).setValue(event.value)
+  }
+  OnStateDeSelect(event) {
+    this.formElement.get(['address', 'stateId']).setValue('')
+    this.formElement.get(['address', 'stateObj']).setValue('')
+    this.formElement.get(['address', 'city']).setValue('');
+    this.formElement.get(['address', 'zipCode']).setValue('');
+    this.selectedState = this.defaultList;
   }
   upateUrl = () => {
     let urlStr = (this.formControl['name'].value).split(' ').join('_')
@@ -130,14 +161,6 @@ export class OrgBasicComponent implements OnInit {
       this.isCopied = false;
     }, 2000);
   }
-  onChangeCountry(event) {
-    let id = event.value
-    this.formElement.get(['address', 'countryId']).setValue(event.value)
-    this.getStateList(id)
-  }
-  onChangeState(event) {
-    this.formElement.get(['address', 'stateId']).setValue(event.value)
-  }
   myUploader = (event, control) => {
     this.fileData = <File>event.files[0];
     let url = ApiPath.documentUpload
@@ -152,24 +175,31 @@ export class OrgBasicComponent implements OnInit {
     let postObj = {
       ...this.formElement.value
     }
-    postObj.address.countryObj = postObj.address.countryObj[0];
-    postObj.address.stateObj = postObj.address.stateObj[0];
-    debugger
+    postObj.address.countryObj = this.formElement.get(['address', 'countryObj']).value[0];
+    postObj.address.stateObj = this.formElement.get(['address', 'stateObj']).value[0];
     if (this.formElement.valid) {
       this.submitted = false;
-      this._HttpService.httpCall(url, 'PUT', postObj, null).subscribe(res => {
-        if (res.result) {
+      this._HttpService.httpCall(url, 'POST', postObj, null).subscribe(res => {
+        if (res && res.responseCode == 406) {
+          /* server side validation mesages */
+          let msgArray = [
+            { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
+          ]
+          this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Message')
+        } else if (res && res.responseCode == 200) {
+          /* success  */
           let msgArray = [
             {
-              mgs: res.responseMessege ? res.responseMessege : 'Success',
+              mgs: res && res.responseMessege ? res.responseMessege : 'Success',
               class: 'confirmMsg'
             },
           ]
           this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Sucess');
           this.fetchData();
         } else {
+          /* any other error */
           let msgArray = [
-            { mgs: 'Something went wrong', class: 'confirmMsg' }
+            { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
           ]
           this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Error')
         }
