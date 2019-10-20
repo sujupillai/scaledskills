8c05e71b7889e6baf21969be0430fef4f470b0eb
himanshu_pandey1 <himanshu.pandey@icreon.com>
2019-10-20 14:29:30 +0530

    save

--- web/src/app/view/training-url/training-url.component.ts
+++ web/src/app/view/training-url/training-url.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiPath } from 'src/app/_helpers/_constants/api';
-import {HttpService} from '../../_service';
+import {HttpService, AuthenticationService, SharedService} from '../../_service';
 @Component({
   selector: 'app-training-url',
   templateUrl: './training-url.component.html',
@@ -18,7 +18,9 @@ export class TrainingUrlComponent implements OnInit {
   urlString:string='';
   entity={};
   userId=0;
-  constructor(private _ActivatedRoute: ActivatedRoute, private _Router: Router, private _HttpService:HttpService) {
+  userInfo: any = {};
+  isLoggedIn: boolean = false;
+  constructor(private _ActivatedRoute: ActivatedRoute, private _Router: Router, private _HttpService:HttpService, private _AuthenticationService: AuthenticationService, private _SharedService:SharedService) {
   }
   ngOnInit() {
     this.noRecord = [
@@ -73,6 +75,36 @@ export class TrainingUrlComponent implements OnInit {
       }
     })
   }
+  fetchUserInfo = (item) => {
+    this.userInfo = this._AuthenticationService.currentUserValue
+    this.isLoggedIn = this.userInfo ? true : false;
+    if (this.isLoggedIn) {
+      let url = ApiPath.interest;
+      url = url.replace('{TrainingId}', item.trainingId.toString())
+      this._HttpService.httpCall(url, 'POST', item.trainingId, null).subscribe(res => {
+        if (res && res.responseCode == 200) {
+          let msgArray = [
+            { mgs: res && res.responseMessege ? res.responseMessege : 'Success', class: 'confirmMsg' }
+          ]
+          this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Success')
+          item.interestCount = item.interestCount + 1;
+          item.isInterested = true;
+        } else {
+          let msgArray = [
+            { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
+          ]
+          this._SharedService.dialogConfig(msgArray, false, false, false, null, null, true, 'Error')
+        }
+      })
+    } else {
+      localStorage.setItem('returnurl', this._Router.url);
+      this._Router.navigate(['/auth/login']);
+    }
+  }
+  handleInterest = (item) => {
+    this.fetchUserInfo(item)
+  };
+
   showDialog() {
     this.display = true;
   }
