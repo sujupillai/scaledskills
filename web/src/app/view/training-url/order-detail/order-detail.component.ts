a38d94a80dfca818f48a7109693bbf52a8e5d34c
himanshu0000 <himanshu.p89@gmail.com>
2020-03-07 19:33:47 +0530

    repo

--- web/src/app/view/training-url/order-detail/order-detail.component.ts
+++ web/src/app/view/training-url/order-detail/order-detail.component.ts
@@ -32,21 +32,45 @@ export class OrderDetailComponent implements OnInit {
     let url = ApiPath.ordersSummary;
     url = url.replace('{orderId}', this.urlString.toString())
     this._HttpService.httpCall(url, 'GET', null, null).subscribe(res => {
-      this.orderSummaryData = res['result']
+      this.orderSummaryData = res['result'] ? res['result'] : null
     })
   }
   _httpOrder = (orderAppId) => {
+    debugger
     let url = ApiPath.placeOrder;
     url = url.replace('{orderId}', this.urlString.toString())
     this._HttpService.httpCall(url, 'POST', orderAppId, null).subscribe(res => {
+      debugger
+      // if (res && res.responseCode == 406) {
+      //   let msgArray = [
+      //     { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
+      //   ]
+      //   this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Message')
+      // } else if (res && res.responseCode == 200) {
+      //   /* success  */
+      //   let msgArray = [
+      //     {
+      //       mgs: 'Your booking has been confirmed. Check your email for detials.',
+      //       class: 'confirmMsg'
+      //     },
+      //   ]
+      //   this._SharedService.dialogConfig(msgArray, true, true, false, 'OK', null, false, 'Sucess').subscribe(res => {
+      //     let returnUrl = localStorage.getItem('returnurl') || '/';
+      //     this._Router.navigate([returnUrl]);
+      //   })
+      // } else {
+      //   /* any other error */
+      //   let msgArray = [
+      //     { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
+      //   ]
+      //   this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Error')
+      // }
       if (res && res.responseCode == 406) {
-        /* server side validation mesages */
         let msgArray = [
           { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
         ]
         this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Message')
-      } else if (res && res.responseCode == 200) {
-        /* success  */
+      } else {
         let msgArray = [
           {
             mgs: 'Your booking has been confirmed. Check your email for detials.',
@@ -57,39 +81,12 @@ export class OrderDetailComponent implements OnInit {
           let returnUrl = localStorage.getItem('returnurl') || '/';
           this._Router.navigate([returnUrl]);
         })
-      } else {
-        /* any other error */
-        let msgArray = [
-          { mgs: res && res.responseMessege ? res.responseMessege : 'Something went wrong', class: 'confirmMsg' }
-        ]
-        this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Error')
       }
     }, error => {
       let msgArray = [
         { mgs: error['message'] ? error['message'] : 'Server Error', class: 'confirmMsg' },
       ]
       this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Error')
-    })
-  }
-  paymenMsg = () => {
-    let msgArray = [
-      {
-        mgs: 'Sorry for the inconvenience, our payment integration system is on maintenance mode.', class: 'confirmMsg'
-      },
-      {
-        mgs: `<p>Kindly do the payment in the below company details and call @ +919599350701 for confirmation. Once payment is verified by Scaled Skills Team, your name will be listed under registered users in training page.</p>
-      <p><strong>Bank Name</strong> : ICICI Bank</p>
-      <p><strong>Account Holder's name</strong> : SCALED SKILLS</p>
-      <p><strong>Account Number</strong> : 061005003883</p>
-      <p><strong>IFSC Code</strong> : ICIC0000610</p>
-      <p><strong>UPI</strong> : scaledskills@icici</p>
-      <p><strong>Pan</strong> : APJPJ2150A</p>
-      <p><strong>GST</strong>  : 07APJPJ2150A1ZH </p>`, class: 'subMsg'
-      },
-    ]
-    this._SharedService.dialogConfig(msgArray, true, true, false, 'OK', null, false, 'Sucess').subscribe(res => {
-      let returnUrl = localStorage.getItem('returnurl') || '/';
-      this._Router.navigate([returnUrl]);
     })
   }
   postToExternalSite(orderAppId): void {
@@ -125,15 +122,12 @@ export class OrderDetailComponent implements OnInit {
       if (res) {
         if (this.orderDetail.items[0].totalPrice > 0) {
           this.postToExternalSite(orderAppId);
-          // this.paymenMsg();
         } else {
           this._httpOrder(orderAppId)
         }
       }
     })
   }
-
-
   _httpOrderCancel = (orderAppId) => {
     let url = ApiPath.orderCancel;
     url = url.replace('{orderId}', this.urlString.toString())
@@ -167,7 +161,6 @@ export class OrderDetailComponent implements OnInit {
       this._SharedService.dialogConfig(msgArray, false, false, false, null, null, false, 'Error')
     })
   }
-
   cancelOrder = (orderAppId) => {
     let msgArray = [
       { mgs: 'Are you sure to cancel your order ?', class: 'confirmMsg' },
