0ec1612579db6a301e3d60280fefb295b2127e50
himanshu0000 <himanshu.p89@gmail.com>
2020-04-24 02:25:18 +0530

    training

--- web/src/app/account/trainer/add-training/add-training.component.ts
+++ web/src/app/account/trainer/add-training/add-training.component.ts
@@ -7,19 +7,30 @@ import { ActivatedRoute } from '@angular/router';
 export class AddTrainingComponent implements OnInit {
   tabArray = [];
   active: boolean = true;
+  trainingId;
   constructor(private _ActivatedRoute: ActivatedRoute) { }
   ngOnInit() {
     this.tabArray = [
-      { label: 'Basic', routerLink: 'basic' },
-      { label: 'Location', routerLink: 'location' },
-      { label: 'Tags', routerLink: 'tags' },
-      { label: 'Images', routerLink: 'images' },
+      { label: 'Basic', routerLink: 'basic', idRequired: false },
+      { label: 'Location', routerLink: 'location', idRequired: true },
+      { label: 'Tags', routerLink: 'tags', idRequired: true },
+      { label: 'Images', routerLink: 'images', idRequired: true },
       // { label: 'Ticket', routerLink: 'ticket' },
       // { label: 'Custom Questions', routerLink: 'questions' },
       // { label: 'Promotions', routerLink: 'promotions' },
-      { label: 'Settings', routerLink: 'settings' },
-      { label: 'Add Trainer', routerLink: 'addTrainer' },
-      { label: 'PUBLISH', routerLink: 'publish' },
+      { label: 'Settings', routerLink: 'settings', idRequired: true },
+      { label: 'Add Trainer', routerLink: 'addTrainer', idRequired: true },
+      { label: 'PUBLISH', routerLink: 'publish', idRequired: true },
     ];
+    this._ActivatedRoute.params.subscribe((param: any) => {
+      this.trainingId = param.id;
+      if (this.trainingId == 0) {
+        this.tabArray.map((item) => {
+          if (item.idRequired) {
+            item.isDisabled = true
+          }
+        })
+      }
+    });
   }
 }
