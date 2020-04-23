1341e2d875300f4edd09a224b49d3466935c36a6
himanshu0000 <himanshu.p89@gmail.com>
2020-04-24 02:45:53 +0530

    save

--- web/src/app/account/trainer/add-training/add-training.component.ts
+++ web/src/app/account/trainer/add-training/add-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiPath } from '../../../_helpers/_constants/api';
+import { HttpService } from 'src/app/_service';
 @Component({
   selector: 'app-add-training',
   templateUrl: './add-training.component.html'
@@ -8,7 +10,9 @@ export class AddTrainingComponent implements OnInit {
   tabArray = [];
   active: boolean = true;
   trainingId;
-  constructor(private _ActivatedRoute: ActivatedRoute) { }
+  trainingData = null;
+  isPublished = false;
+  constructor(private _ActivatedRoute: ActivatedRoute, private _HttpService: HttpService) { }
   ngOnInit() {
     this.tabArray = [
       { label: 'Basic', routerLink: 'basic', idRequired: false },
@@ -20,7 +24,7 @@ export class AddTrainingComponent implements OnInit {
       // { label: 'Promotions', routerLink: 'promotions' },
       { label: 'Settings', routerLink: 'settings', idRequired: true },
       { label: 'Add Trainer', routerLink: 'addTrainer', idRequired: true },
-      { label: 'PUBLISH', routerLink: 'publish', idRequired: true },
+      { label: 'PUBLISH', routerLink: 'publish', idRequired: true, isPublisedReq: true },
     ];
     this._ActivatedRoute.params.subscribe((param: any) => {
       this.trainingId = param.id;
@@ -30,7 +34,20 @@ export class AddTrainingComponent implements OnInit {
             item.isDisabled = true
           }
         })
+      } else {
+        this.getData(this.trainingId);
       }
     });
+  }
+  getData = (id) => {
+    this.trainingData = null;
+    let url = ApiPath.getTraining
+    url = url.replace('{id}', id)
+    this._HttpService.httpCall(url, 'GET', null, null).subscribe(res => {
+      if (res.result) {
+        this.trainingData = res.result;
+        this.isPublished = this.trainingData.trainingStatus == 1 ? true : false;
+      }
+    })
   }
 }
